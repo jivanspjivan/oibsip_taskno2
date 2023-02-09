e81e6194820075ea81cf3cfedc5a0c7abf32440f
jivanspjivan <100034284+jivanspjivan@users.noreply.github.com>
2023-02-09 08:43:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// ===============TYPING ANIMATION ==========//
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web developer","graphic designer","youTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
